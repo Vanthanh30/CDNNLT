1151c93fefc05e9fc6400c80e85dfb6ad0300e48
VinTent26 <155293495+VinTent26@users.noreply.github.com>
2026-05-17 18:42:15 +0700

    done docker

--- backend/crawler-service/src/database.py
+++ backend/crawler-service/src/database.py
@@ -5,15 +5,16 @@ import mysql.connector
 from mysql.connector import Error
 from datetime import date, datetime
 from mysql.connector import Error
+import os
 
-DB_HOST = "localhost"
-DB_PORT = 3306
-DB_USER = "root"
-DB_PASSWORD = "123456"
-DB_NAME = "disease_management"
-
+DB_HOST = os.getenv("DB_HOST", "db")
+DB_PORT = int(os.getenv("DB_PORT", 3306))
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "123456")
+DB_NAME = os.getenv("DB_NAME", "disease_management")
 
 def _get_valid_sets():
+    # pyrefly: ignore [missing-import]
     from nlp_engine import VALID_LOCATIONS, VALID_DISEASES
 
     return VALID_LOCATIONS, VALID_DISEASES
